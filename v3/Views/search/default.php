e62086d2d7b51a3ff445d872828f352d7b5547f0
Tim <papplebanana1@gmail.com>
2021-11-04 10:26:00 -0400

    Update

--- v3/Views/search/default.php
+++ v3/Views/search/default.php
@@ -3,10 +3,26 @@
     Ihre Suche nach <b><?php echo $this->getData('query'); ?></b> ergab <b><?php echo count($this->getData('result')); ?></b> Treffer
 </p>
 
-    <?php foreach($this->getData('result') as $book): ?>
+
+
+    <?php 
+    
+    
+
+    foreach($this->getData('result') as $book): ?>
     <li class="list-group-item">
         <a href="/DiesUndDas/v3/book/detail?id=<?php echo $book->getId(); ?>">
         <?php echo $book->getName(); ?>
         </a>
     </li>
-    <?php endforeach; ?>
+    <?php endforeach; 
+    
+
+    if(count($this->getData('result')) == 0){
+       echo(
+        '<div class="alert alert-warning" role="alert">
+        Es wurden keine Ergebnisse gefunden
+        </div>'
+    );
+}
+    ?>
