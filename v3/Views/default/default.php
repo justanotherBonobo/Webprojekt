8e1788fd930e06af15c69b52b44722698c1a57a7
Tim <papplebanana1@gmail.com>
2021-11-07 15:43:35 -0500

    Added Logo

--- v3/Views/default/default.php
+++ v3/Views/default/default.php
@@ -1,10 +1,10 @@
 
 <div class="px-4 py-5 my-5 text-center bg-light rounded-3">
 
-    <img class="d-block mx-auto mb-4" src="/DiesUndDas/v3/data/images/718_schwarz.jpg" alt="" width="240">
+    <img class="d-block mx-auto mb-4" src="/DiesUndDas/v3/assets/logo.png" alt="" width="400">
     
-    <h1 class="display-5 fw-bold">Welcome to the Framework</h1>
+    <h1 class="display-5 fw-bold">Herzlich willkommen</h1>
     <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4">This is the demo of a simple PHP framework showing the MVC pattern</p>
+      <p class="lead mb-4">Wir bieten Ihnen eine große Auswahl an Neu- und Gebrauchtfahrzeugen.</p>
     </div>
   </div>
